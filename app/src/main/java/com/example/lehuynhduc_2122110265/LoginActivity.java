c387e67063cd6a08d41ee3c97e281730d273538e
Le Huynh Duc <hsthptbb.lhduc12a13@gmail.com>
2025-08-06 16:25:30 +0700

    Initial commit

--- app/src/main/java/com/example/lehuynhduc_2122110265/LoginActivity.java
+++ app/src/main/java/com/example/lehuynhduc_2122110265/LoginActivity.java
@@ -1,10 +1,10 @@
 package com.example.lehuynhduc_2122110265;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -17,13 +17,37 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_login);
 
-        Button btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
+        // Tránh giao diện tràn status bar
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
 
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText objEmail = findViewById(R.id.txtEmail);
+                String txtEmail = objEmail.getText().toString();
+
+                EditText objPass = findViewById(R.id.txtPass);
+                String txtPass = objPass.getText().toString();
+
+                if (txtEmail.equals("duc") && txtPass.equals("123"))
+                {
+                    Intent it = new Intent(getApplicationContext(), MainActivity.class);
+
+                    it.putExtra("duc",txtEmail);
+                    it.putExtra("pass",txtPass);
+                    startActivity(it);
+                } else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Login fail", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/lehuynhduc_2122110265/RegisterActivity.java
+++ app/src/main/java/com/example/lehuynhduc_2122110265/RegisterActivity.java
@@ -1,8 +1,6 @@
 package com.example.lehuynhduc_2122110265;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        // Tránh tràn lên status bar
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button btnBack = findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
-        });
-        Intent intent = getIntent();
-        String txtEmail = intent.getStringExtra("duc");
-        String txtass = intent.getStringExtra("pass");
     }
 }
